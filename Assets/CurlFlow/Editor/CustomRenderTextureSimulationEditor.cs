d5dd478475a1c4d5980728d7c4200f58cb4b715c
tirolinko <emk.tonev@gmail.com>
2026-02-24 13:13:13 +0200

    First basic implementation - quad with moving kernels but no color reintroduction

---
+++ Assets/CurlFlow/Editor/CustomRenderTextureSimulationEditor.cs
@@ -0,0 +1,69 @@
+﻿using CurlFlow.Runtime;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CurlFlow.Editor
+{
+    [CustomEditor(typeof(CustomRenderTextureSimulation))]
+    public class CustomRenderTextureSimulationEditor : UnityEditor.Editor
+    {
+        private MaterialEditor m_materialEditor;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = new VisualElement();
+            
+            AddRestartButton(root);
+
+            InspectorElement.FillDefaultInspector(root, serializedObject, this);
+            
+            // This draws the custom render texture material inspector below the simulation component, which
+            // allows us to adjust the material without having to select the render texture asset every time
+            var customRenderTextureProperty = serializedObject.FindProperty("m_customRenderTexture");
+            var customRenderTexture = customRenderTextureProperty.objectReferenceValue as CustomRenderTexture;
+
+            if (customRenderTexture == null || customRenderTexture.material == null)
+                return root;
+            
+            AddRenderTextureMaterialInspector(customRenderTexture, root);
+
+            return root;
+        }
+
+        private void AddRenderTextureMaterialInspector(CustomRenderTexture customRenderTexture, VisualElement root)
+        {
+            var material = customRenderTexture.material;
+            if (m_materialEditor == null)
+            {
+                m_materialEditor = UnityEditor.Editor.CreateEditor(material) as MaterialEditor;
+            }
+            
+            var container = new IMGUIContainer(() =>
+            {
+                m_materialEditor.DrawHeader();
+                EditorGUILayout.BeginVertical();
+                m_materialEditor.OnInspectorGUI();
+                EditorGUILayout.EndVertical();
+            });
+            
+            container.style.backgroundColor = new Color(0.2f, 0.2f, 0.2f, 1f);
+            
+            root.Add(container);
+        }
+
+        private void AddRestartButton(VisualElement root)
+        {
+            var button = new Button(() =>
+            {
+                var simulation = target as CustomRenderTextureSimulation;
+                simulation.RestartSimulation();
+            })
+            {
+                text = "Restart Simulation"
+            };
+            root.Add(button);
+        }
+    }
+}
---
+++ Assets/CurlFlow/Runtime/CustomRenderTextureSimulation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CurlFlow.Runtime
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class CustomRenderTextureSimulation : MonoBehaviour
+    {
+        [SerializeField] private CustomRenderTexture m_customRenderTexture;
+        
+        private MeshRenderer m_meshRenderer;
+        
+        private void Awake()
+        {
+            m_meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            if (m_meshRenderer.isVisible)
+            {
+                m_customRenderTexture.Update();
+            }
+        }
+
+        [ContextMenu("Restart Simulation")]
+        public void RestartSimulation()
+        {
+            m_customRenderTexture.Initialize();
+        }
+    }
+}
